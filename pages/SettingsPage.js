592ccd60255f29295284683e5a1adb9f231c4801
Web4 <167559384+Web4application@users.noreply.github.com>
2025-04-12 19:33:41 -0400

    Create SettingsPage.js

---
+++ pages/SettingsPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SettingsPage = () => {
+  return (
+    <div className="settings-page">
+      <h2>Settings</h2>
+      {/* Render user settings options */}
+    </div>
+  );
+};
+
+export default SettingsPage;

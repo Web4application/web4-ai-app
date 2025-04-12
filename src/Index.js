55229195e9686d11f30f9e9357dbe19d931725b2
Web4 <167559384+Web4application@users.noreply.github.com>
2025-04-12 19:17:50 -0400

    Update Index.js

--- src/Index.js
+++ src/Index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './styles.css';
 import ChatApp from './ChatApp';
 
-// Render the ChatApp component into the root div
 ReactDOM.render(
   <React.StrictMode>
     <ChatApp />

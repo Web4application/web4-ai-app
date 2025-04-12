eba6d2f3d532b4d165a0011985823407c0487596
Web4 <167559384+Web4application@users.noreply.github.com>
2025-04-12 19:32:54 -0400

    Create ProfilePage.js

---
+++ pages/ProfilePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ProfilePage = () => {
+  return (
+    <div className="profile-page">
+      <h2>User Profile</h2>
+      {/* Render user profile info here */}
+    </div>
+  );
+};
+
+export default ProfilePage;
